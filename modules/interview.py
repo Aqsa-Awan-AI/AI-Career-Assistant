ee35635e89da82c7bad5879a6d4e62de1c886061
Aqsa Awan <aaqsa8891@gmail.com>
2026-08-04 15:46:00 +0500

    Add AI Interview Coach

--- modules/interview.py
+++ modules/interview.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from google import genai
+import os
 
 QUESTIONS = {
     "Python": [
@@ -30,6 +32,44 @@ QUESTIONS = {
         "Where do you see yourself in 5 years?"
     ]
 }
+def generate_ai_interview(role, experience, interview_type):
+
+    client = genai.Client(
+        api_key=os.getenv("GEMINI_API_KEY")
+    )
+
+    prompt = f"""
+    You are an expert technical interviewer.
+
+    Generate a professional interview for:
+
+    Job Role: {role}
+    : {experience}
+    Interview Type: {interview_type}
+
+    Provide:
+
+    # Interview Questions
+
+    For each of the 5 questions include:
+
+    Question:
+    Ideal Answer:
+    Interview Tip:
+
+    Finally provide:
+
+    # Overall Interview Advice
+
+    Return everything in Markdown.
+    """
+
+    response = client.models.generate_content(
+        model="gemini-3.5-flash",
+        contents=prompt
+    )
+
+    return response.text
 
 
 def interview_page():
@@ -47,3 +87,49 @@ def interview_page():
 
     for i, question in enumerate(QUESTIONS[category], start=1):
         st.info(f"{i}. {question}")
+    st.divider()
+
+st.subheader("🤖 AI Interview Coach")
+
+job_role = st.text_input(
+    "Job Role",
+    placeholder="e.g. AI Engineer"
+)
+
+experience = st.selectbox(
+    "Experience Level",
+    [
+        "Fresher",
+        "1-3 Years",
+        "3-5 Years",
+        "5+ Years"
+    ]
+)
+
+interview_type = st.selectbox(
+    "Interview Type",
+    [
+        "Technical",
+        "HR",
+        "Behavioral",
+        "Mixed"
+    ]
+)
+
+if st.button("🚀 Generate AI Interview"):
+
+    if job_role.strip() == "":
+        st.warning("Please enter a job role.")
+    else:
+
+        with st.spinner("Generating Interview..."):
+
+            result = generate_ai_interview(
+                job_role,
+                experience,
+                interview_type
+            )
+
+        st.success("Interview Generated Successfully!")
+
+        st.markdown(result)
